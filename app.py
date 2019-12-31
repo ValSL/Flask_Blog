d2a4274face270d083a515e2de7919c943af9f81
ValSL <valid.saley@gmail.com>
2019-12-31 17:35:20 +0300

    admin obj, two admin viewes, secretKey

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+# 1) Импорт класса админ
+from flask_admin import Admin
+# 3) Импорт класса ModelView для наших моделей БД
+from flask_admin.contrib.sqla import ModelView
+
+
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Configuration)
 
@@ -13,3 +19,17 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)  # (название команды, команда)
+
+
+# 5) Наши модели
+from models import *
+
+# Admin #
+# 2) Создание экземпляра класса Admin
+admin = Admin(app)
+
+# 4) Добвавление в админку нашу таблицу Post
+admin.add_view(ModelView(Post, db.session))
+
+# 6) Добвавление в админку нашу таблицу Tag (потом в конфиг)
+admin.add_view(ModelView(Tag, db.session))
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://valsl:etereg14@localhost/test1'
+    # 7) Добавление Secret Key
+    SECRET_KEY = 'secret'
