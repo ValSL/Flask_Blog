f3cca243893e7fc28d86a0ca101d4444af74359a
ValSL <valid.saley@gmail.com>
2019-12-31 18:10:29 +0300

    User and Role models

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://valsl:etereg14@localhost/test1'
-    # 7) Добавление Secret Key
+    # 7) Добавление Secret Key (Далее создание моделей)
     SECRET_KEY = 'secret'
